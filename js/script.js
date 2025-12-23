5d80913fe7db0fabfd75157e3562cdbed2cd84c5
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-23 18:21:42 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-const display = document.getElementById('txt-imsa')
-const dataAtual = new Date()
-const dataAlvo = new Date(2026, 0, 24)
-const diferenca = dataAlvo - dataAtual
-const dias = Math.floor(diferenca / 86400000)
-display.innerHTML = `Faltam ${dias} dias!`
+function regressiva(diaEvento, idElemento) {
+    const display = document.getElementById(idElemento)
+    const intervalo = setInterval(() => {
+        const dataAtual = new Date()
+        const diferenca = diaEvento - dataAtual
+        const dias = Math.floor(diferenca / 86400000)
+        display.textContent = `Faltam ${dias} dias!`
+    }, 1000)
+}
+regressiva(new Date(2026, 0, 24), 'txt-imsa')
