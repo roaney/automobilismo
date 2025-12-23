c4b024890f432af9e5e240f0e59ac06e7dd74e1b
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-23 17:42:46 -0300

    update

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ const dataAtual = new Date()
 const dataAlvo = new Date(2026, 0, 24)
 const diferenca = dataAlvo - dataAtual
 const dias = Math.floor(diferenca / 86400000)
-display.textContent = `Faltam ${dias} dias`
+display.innerHTML = `Faltam ${dias} dias!`
