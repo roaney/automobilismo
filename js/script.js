5c74eea73b0cf63cbaa8bfef56f1066468696433
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-23 21:17:58 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,16 @@ function regressiva(diaEvento, idElemento) {
     const intervalo = setInterval(() => {
         const dataAtual = new Date()
         const diferenca = diaEvento - dataAtual
-        const dias = Math.floor(diferenca / 86400000)
-        display.textContent = `Faltam ${dias} dias!`
+        const dias = Math.ceil(diferenca / 86400000)
+        if (dias == 1) {
+            display.textContent = 'É amanhã!'
+        } else if (diferenca <= 0 && diferenca >= -86400000) {
+            display.textContent = 'É hoje!'
+        } else if (diferenca < -86400000) {
+            display.textContent = 'Evento encerrado!'
+        } else {
+            display.textContent = `Faltam ${dias} dias!`
+        }
     }, 1000)
 }
 regressiva(new Date(2026, 0, 24), 'txt-imsa')
