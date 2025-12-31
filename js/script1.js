338f617f888b597f3d53ca713165f727b95bac2f
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-31 11:17:09 -0300

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -2,8 +2,8 @@ const statusCarro = {
     pneus: "Médios",
     combustivel: "40%",
     temperatura: "95°C"
-};
-const alertas = Object.entries(statusCarro).filter(([chave, valor]) => {
-    return valor.includes("%") || valor.includes("°C");
-});
+}
+const alertas = Object.values(statusCarro).filter(valor => {
+    return valor.includes("%") || valor.includes("°C")
+})
 console.log(alertas)
