cd7fa02f4c9f22ce220901c2c540b29aabb0268b
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-31 18:31:50 -0300

    Delete script1.js

--- js/script1.js
+++
@@ -1,9 +0,0 @@
-const statusCarro = {
-    pneus: "Médios",
-    combustivel: "40%",
-    temperatura: "95°C"
-}
-const alertas = Object.values(statusCarro).filter(valor => {
-    return valor.includes("%") || valor.includes("°C")
-})
-console.log(alertas)
