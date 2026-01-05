3b1cb22d630c2375c7285f29c15247db1d4af405
Roaney Faria Adami <roaney91@hotmail.com>
2026-01-05 14:44:10 -0300

    Update agenda-semanal.js

--- js/agenda-semanal.js
+++ js/agenda-semanal.js
@@ -12,41 +12,38 @@ botaoFechar.addEventListener('click', () => {
     overlay.style.visibility = 'hidden'
     overlay.style.opacity = '0'
 })
-const f1 = [
-    ['Fórmula 1', 'Austrália', new Date(2026, 0, 5)],
-    ['Fórmula 1', 'China', new Date(2026, 0, 11)],
-    ['Fórmula 1', 'Japão', '29 de março'],
-    ['Fórmula 1', 'Barein', '12 de abril'],
-    ['Fórmula 1', 'Arábia Saudita', '19 de abril']
-]
-const nascar = [
-    ['Nascar', 'Bowman Gray', new Date(2026, 0, 5)],
-    ['Nascar', 'Daytona Duels', new Date(2026, 0, 11)],
-    ['Nascar', 'Daytona 500', '15 de fevereiro'],
-    ['Nascar', 'Atlanta', '22 de fevereiro'],
-    ['Nascar', 'COTA', '1 de março']
-]
+const calendario = {
+    'Fórmula 1': [
+        ['Austrália', new Date(2026, 0, 4)],
+        ['China', new Date(2026, 0, 12)]
+    ],
+    'Nascar': [
+        ['Bowman Gray', new Date(2026, 0, 19)],
+        ['Daytona Duels', new Date(2026, 0, 25)]
+    ]
+}
 const h1 = document.getElementById('h1-agenda')
 const agenda = document.getElementById('agenda-semanal')
-const todasCategorias = [f1, nascar]
-function filtroSemanal (categoria) {
-    const hoje = new Date()
-    let diaDaSemana = hoje.getDay()
-    if (diaDaSemana == 0) {
-        diaDaSemana = 7
-    }
-    const segunda = new Date()
-    segunda.setDate(hoje.getDate() - (diaDaSemana - 1))
-    const domingo = new Date()
-    domingo.setDate(hoje.getDate() + (7 - diaDaSemana))
-    const categoriaFiltrada = categoria.filter(([nome, local, data]) => {
+const hoje = new Date(2026, 0, 30)
+let diaDaSemana = hoje.getDay()
+if (diaDaSemana == 0) {
+    diaDaSemana = 7
+}
+const segunda = new Date()
+segunda.setDate(hoje.getDate() - (diaDaSemana - 1))
+segunda.setHours(-1)
+const domingo = new Date()
+domingo.setDate(hoje.getDate() + (7 - diaDaSemana))
+domingo.setHours(1)
+const categorias = Object.entries(calendario)
+categorias.forEach(([categoria, etapas]) => {
+    const etapaDaSemana = etapas.filter(([local, data]) => {
         return data >= segunda && data <= domingo
     })
-    categoriaFiltrada.forEach(([nome, local, data]) => {
-        agenda.innerHTML += `${nome} - ${local} - ${String(data.getDate()).padStart(2, '0')} / ${String(data.getMonth()+1).padStart(2, '0')}<br>`
+    etapaDaSemana.forEach(([local, data]) => {
+        agenda.innerHTML += `${categoria} - ${local} - ${String(data.getDate()).padStart(2, '0')}/${String(data.getMonth() + 1).padStart(2, '0')}<br>`
     })
-}
-todasCategorias.forEach(elemento => filtroSemanal(elemento))
+})
 if (agenda.textContent.trim() === '') {
     h1.innerHTML = '<i class="fa-regular fa-face-frown"></i> Que pena, não temos corridas nesse final de semana.'
 } else {
